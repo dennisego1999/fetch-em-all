484ad32a54ab08ad5416b5bb4b4b04b9f2eeb5bf
Dennis Ego <dennis.ego@artcoresociety.com>
2026-03-05 14:28:51 +0100

    Add video component

---
+++ src/js/Components/Molecules/Video/Video.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  href: string;
+}>();
+</script>
+
+<template>
+  <iframe class="video" :src="href" frameborder="0"></iframe>
+</template>
--- src/js/Pages/Home.vue
+++ src/js/Pages/Home.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import SectionOnboarding from "../Components/Organisms/SectionOnboarding/SectionOnboarding.vue";
+import Video from "@/js//Components/Molecules/Video/Video.vue";
+import SectionOnboarding from "@/js//Components/Organisms/SectionOnboarding/SectionOnboarding.vue";
 </script>
 
 <template>
   <SectionOnboarding />
+
+  <Video
+    data-aspect-ratio="video"
+    href="https://www.youtube.com/embed/DX1aBG9AdQU?si=7HBnINLZdtV-HKGY"
+  />
 </template>
